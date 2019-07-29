5ae299acd12127749f7e7498dade60047c88eb79
ruca6616 <ruca6616@gmail.com>
2019-07-29 14:56:05 -0700

    Added unit test project for Account class.

---
+++ BankAccount/Account.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// Represents a customer's bank account
+    /// </summary>
+    public class Account
+    {
+        // name of account holder
+        // account number
+        // balance
+
+        public string AccountNumber { get; set; }
+
+        public string Owner { get; set; }
+
+        // set as private for encapsulation purposes
+        public double Balance { get; private set; }
+
+
+        /// <summary>
+        /// Adds an amount to the current balance and returns the updated balance
+        /// </summary>
+        /// <param name="amount">The amount to add</param>
+        /// <returns>The new balance</returns>
+        public double Deposit( double amount )
+        {
+            if (amount < 0)
+            {
+                // throw exception for fellow developer, nameof() for refactoring support
+                throw new ArgumentException($"{nameof(amount)} cannot be negative");
+            }
+
+            Balance += amount;
+            return Balance;
+        }
+
+        /// <summary>
+        /// Subtracts given amount from current balance
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public double Withdraw( double amount )
+        {
+            Balance -= amount;
+            return Balance;
+        }
+
+
+
+
+
+    }
+}
---
+++ BankAccountTests/AccountTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+
+        [TestMethod()]
+        public void DepositTest_PositiveAmount_AddsToBalance()
+        {
+            // AAA: Arrange, Act, Assert
+
+            // arrange - Init objects/variables
+            Account checking = new Account();
+            double depositAmt = 10;
+            double expectedBalance = 10;
+
+            // act - call method under test
+            checking.Deposit(depositAmt);
+
+            // assert - verify action
+            Assert.AreEqual(expectedBalance, checking.Balance);
+        }
+
+
+        [TestMethod]
+        public void Deposit_PositiveAmount_ReturnsUpdatedBalance()
+        {
+            // arrange
+            Account acc = new Account();
+            double depositAmt = 10.55;
+            double expectedReturn = 10.55;
+
+            // act
+            double result = acc.Deposit(depositAmt);
+
+            // assert
+            Assert.AreEqual(expectedReturn, result);
+        }
+
+
+        [TestMethod]
+        public void Deposit_NegativeAmount_ThrowsArgumentException()
+        {
+            // arrange
+            Account acc = new Account();
+            double depositAmt = -1;
+
+            // act => assert: in this case - really assert then act
+            // using anonymous function, this should throw the exception 
+            Assert.ThrowsException<ArgumentException>( () => acc.Deposit(depositAmt) );
+
+
+        }
+
+
+    }
+}
